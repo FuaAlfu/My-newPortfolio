ccfc3036f9fa7ad2ba8702899f19a7b3666c2c46
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2024-03-06 14:31:16 +0300

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,33 @@ import {aboutMeData} from './data/data';
 
 const AboutMePage = () =>{
     return (
-      <main>
-          <section>
-              <div>Image</div>
-              <div> Text</div>
+      <main className='flex flex-col items-center'>
+          <section className='flex flex-row w-10/12 mt-10 mb-6 items-center'>
+              <div className='w-1/3'>
+                  <Image 
+                  priority={false}
+                  width={640}
+                  height={960} 
+                  aria-label={"Image of .."} 
+                  alt={"Image of .."}
+                  src={'/images/..'}
+                  />
+              </div>
+              <div> 
+                  <h2>{aboutMeData.title}</h2>
+                  <p>{aboutMeData.body}</p>
+              </div>
           </section>
           {/*  */}
-          <section></section>
+          <section>
+              <p>{aboutMeData.highlightedBody}</p>
+          </section>
           {/*  */}
           <section>
-              <div>Image</div>
-              <div> Text</div>
+              <div>
+                  <p>{aboutMeData.body2}</p>
+              </div>
+              <div> Image</div>
           </section>
       </main>
     )
